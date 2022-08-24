e94956508d952e228824efcadde62172e0199db3
mannuel25 <oluwasegunprosperity@gmail.com>
2022-08-24 20:31:13 +0100

    convert temperature to celcius

--- weather/temperature/views.py
+++ weather/temperature/views.py
@@ -5,15 +5,16 @@ import urllib.request  #to make request to api
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-
+        
         source = urllib.request.urlopen(
              'https://api.openweathermap.org/data/2.5/weather?q='
                     + city + '&appid=make_sure_to_add_your_unique_api_key_here').read()
 
         temp_data = json.loads(source)
-
+        temp_in_kelvin = str(temp_data['main']['temp']) + ' K'
+        temp_in_celcius = str(round((temp_data['main']['temp'] -  273.15),2)) + ' C'
         data = {
-            "temp": str(temp_data['main']['temp']) + ' K',
+            "temp": temp_in_kelvin + ' / ' + temp_in_celcius
         }
         print(data)
     else:
